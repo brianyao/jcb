4c35aee5882e4e489b0d3c203d5d0fa154b3b744
sc <sc.xiong@qq.com>
2013-07-15 08:47:34 -0700

    preniminary for QQ connect application

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # localhost:3000
   provider :facebook,   '370472209740767', 'c349de4298ca8f533c2a741662648ebc'
   provider :weibo,      '1120491544',      '3c207d5c21f7080759e15ea42dfd2332'
-  provider :qq_connect, '100476877',       '7eda1ece1c68f2bc81d5add294cc4c23'
-  
+  provider :qq_connect, '100480760',       '4d675266d4e6ba32c3dde91c221258e1'
+  # provider :qq_connect, '100476877',       '7eda1ece1c68f2bc81d5add294cc4c23'
   # provider :twitter, '5xviWhmRAkQmGv2mT47Ygg', 'lhlCsDWq3qzPzYYthZAv0BC33SGOchufPG66mpoSbg'
 end
